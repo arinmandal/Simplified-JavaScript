1e7558690bf32bc3fdb727690e35177a74e8600e
Arin <arinmandal40@gmail.com>
2023-10-04 16:48:25 +0530

    number and maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -17,18 +17,29 @@ const hundreds = 1000000
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
+//* Returns the absolute value of a number (the value without regard to whether it is positive or negative). For example, the absolute value of -5 is the same as the absolute value of 5.
+// console.log(Math.abs(-10));
+
+//* Returns a supplied numeric expression rounded to the nearest integer.
 // console.log(Math.round(4.6));
+
+//* Returns the smallest integer greater than or equal to its numeric argument. 5 >= 4.2
 // console.log(Math.ceil(4.2));
+
+//* Returns the greatest integer less than or equal to its numeric argument 4 >= 4.9
 // console.log(Math.floor(4.9));
+
+//* Returns the smaller of a set of supplied numeric expressions.
 // console.log(Math.min(4, 3, 6, 8));
+
+//* Returns the larger of a set of supplied numeric expressions.
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
 
 const min = 10
 const max = 20
 
-// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
