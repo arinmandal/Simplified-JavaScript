4b4720374a2222f38f5a6218a3effa2062e40cfa
Arin <arinmandal40@gmail.com>
2023-10-06 11:26:02 +0530

    IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,22 @@
 // Immediately Invoked Function Expressions (IIFE)
 
 
-(function chai(){
+(function chai() {
     // named IIFE
     console.log(`DB CONNECTED`);
 })();
 
-( (name) => {
+((name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('Arin')
+})('Arin')
 
+
+let val1 = 10;
+let val2 = 5;
+
+function addNum(num1, num2) {
+    let total = (num1 + num2);
+    console.log(total);
+}
+let result1 = addNum(val1, val2);
+let result2 = addNum(10, 2);
