5edff00dc027ee8f9377f0347768ed08230f896d
Arin <arinmandal40@gmail.com>
2023-11-10 18:57:02 +0530

    promise

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -48,24 +48,83 @@
 //   console.log(user)
 // })
 
-const promiseFour = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    const user = true;
-    if (!user) {
-      resolve({ username: "Arin", password: "456" });
-    } else {
-      console.log("ERROR: Something went worng");
-    }
+// const promiseFour = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const user = false;
+//     if (!user) {
+//       resolve({ username: "Arin", password: "456" });
+//     } else {
+//       console.log("ERROR: Something went worng");
+//     }
 
-  }, 1000);
-})
+//   }, 1000);
+// })
 
 
-const user = promiseFour.then((user) => {
-  console.log(user);
-  return user.username;
-}).then((username) => {
-  console.log(username)
-}).catch(() => {
-  console.log()
-})
+// promiseFour.then((user) => {
+//   console.log(user);
+//   return user.username;
+// }).then((username) => {
+//   console.log(username)
+// }).catch(() => {
+//   console.log()
+// }).finally(() => {
+//   console.log('Experiment completed');
+// });
+
+//////////////////////////////////////
+
+// Promise with async / await
+
+// const promiseFive = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let user = false;
+//     if (!user) {
+//       resolve({ username: "JavaScript", password: "mystery" })
+//     } else {
+//       console.log('Error!: JS Error')
+//     }
+//   }, 1000)
+// })
+
+// async function consumePromiseFive() {
+//   try {
+//     const response = await promiseFive
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// consumePromiseFive()
+
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//     const data = await response.json()
+//     console.log(data)
+//   } catch (error) {
+//     console.log('Error:', error);
+//   }
+// }
+
+// getAllUsers()
+
+// //////////////////////////////////////////////
+
+// fetch()
+
+// fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+//   return response.json()
+// }).then((data) => {
+//   console.log(data)
+// }).catch((error) => {
+//   console.log(error);
+// })
+
+console.log('start')
+setTimeout(function cb() {
+  console.log("hello from CB")
+}, 5000);
+
+console.log('end')
--- 09_advance_one/tempCodeRunnerFile.js
+++ 09_advance_one/tempCodeRunnerFile.js
@@ -1,3 +1,11 @@
-promiseThree.then((user) => {
-//   console.log(user)
+const promiseFour = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const user = false;
+//     if (!user) {
+//       resolve({ username: "Arin", password: "456" });
+//     } else {
+//       console.log("ERROR: Something went worng");
+//     }
+
+//   }, 1000);
 // })
