e20cb19681935991f6a827af7ef11af5a4ebc58c
Arin <arinmandal40@gmail.com>
2024-12-20 00:51:05 +0530

    Datatypes

--- JavascriptInfo/JsFundamentals/script.js
+++ JavascriptInfo/JsFundamentals/script.js
@@ -21,3 +21,21 @@ Create a variable to store the name of a current visitor to a website. How would
 let ourPlanetName = 'Earth';
 let currentVisitorName = 'John';
 
+//? Working with Data Types
+// Infinity represents the mathematical Infinity ∞. It is a special value that’s greater than any number.
+// alert(1 / 0); // Output: Infinity
+
+// NaN (Not a Number) is a special value that represents an operation that cannot be performed. For instance:
+// alert("not a number" / 2); // Output: NaN
+
+// Seven primitive data types:
+//* number for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+//* bigint for integer numbers of arbitrary length.
+//* string for strings. A string may have zero or more characters, there’s no separate single-character type.
+//* boolean for true/false.
+//* null for unknown values – a standalone type that has a single value null.
+//* undefined for unassigned values – a standalone type that has a single value undefined.
+//* symbol for unique identifiers.
+//* And one non-primitive data type:
+//* object for more complex data structures.
+
