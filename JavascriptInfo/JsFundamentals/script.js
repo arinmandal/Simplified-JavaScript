5841472406fa03d1d32c69f4002bca008560765a
Arin <arinmandal40@gmail.com>
2024-12-20 00:58:15 +0530

    Feat: alert, prompt, confirm

--- JavascriptInfo/JsFundamentals/script.js
+++ JavascriptInfo/JsFundamentals/script.js
@@ -39,3 +39,11 @@ let currentVisitorName = 'John';
 //* And one non-primitive data type:
 //* object for more complex data structures.
 
+//! Interaction: alert, prompt, confirm
+
+// alert
+// shows a message.
+// prompt
+// shows a message asking the user to input text. It returns the text or, if Cancel button or Esc is clicked, null.
+// confirm
+// shows a message and waits for the user to press “OK” or “Cancel”. It returns true for OK and false for Cancel/Esc.
