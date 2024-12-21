67e28c0f9f6dbb3f8198c05e6e35c2525f2026d1
Arin <arinmandal40@gmail.com>
2024-12-21 20:27:47 +0530

    The "switch" statementt

--- JavascriptInfo/JsFundamentals/script.js
+++ JavascriptInfo/JsFundamentals/script.js
@@ -324,3 +324,77 @@ while( i < 3){
   i++;
 } 
 */
+
+//? The "switch" statement
+// TODO: Task 1
+/* let names = prompt("Enter your name:");
+
+switch (names) {
+  case "John":
+    console.log("Hello, John!");
+    break;
+  case "Jane":
+    console.log("Hello, Jane!");
+    break;
+  case "Hello":
+    console.log("Hello, Hello!");
+    break;
+  default:
+    console.log("Hello, Stranger!");
+} */
+
+// TODO: Task 2
+// rewrite this "switch" into an "if"
+
+// let browser = prompt("Enter your browser name:");
+/* switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+} */
+
+/* if (browser == 'Edge') {
+  alert( "You've got the Edge!" );
+} else if (browser === 'Chrome' || 'Firefox' || 'Safari' || 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
+} */
+
+//TODO: Task 3
+// Rewrite "if" to switch
+// let a = +prompt('a?', '');
+
+/* if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+} */
+
+/* switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert('2, 3');
+    break;
+} */
