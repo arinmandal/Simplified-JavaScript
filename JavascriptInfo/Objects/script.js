ab2174a99f06c76b010346629c3085b3c160cad9
Arin <arinmandal40@gmail.com>
2025-01-07 22:13:39 +0530

    Object

---
+++ JavascriptInfo/Objects/script.js
@@ -0,0 +1,20 @@
+// ? Objects
+// Objects are used to store keyed collections of various data and more complex entities. In JavaScript, objects penetrate almost every aspect of the language. So we must understand them first before going in-depth anywhere else.
+// An object can be created with figure brackets {…} with an optional list of properties. A property is a “key: value” pair, where key is a string (also called a “property name”), and value can be anything.
+
+/* let user = new Object(); // "object constructor" syntax
+let user = {};  // "object literal" syntax */
+
+//? Literals and properties
+
+/* let user = {
+  name: "John", // by key stored the value "John"
+  age: 25 // by key "age" store value 25 
+} */
+
+// get property values of the object:
+console.log(user.name) // John
+console.log(user.age) // 25
+
+
+
